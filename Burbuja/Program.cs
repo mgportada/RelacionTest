2312c1a522c2736ede3e1f3c2d7ccd7acf2b1079
manugp <manuelgarces@iesportada.org>
2024-06-11 12:06:19 +0200

    Agregar archivos de proyecto.

---
+++ Burbuja/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Burbuja
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Entradas
+            int[] sec = { 5, 3, 8, 4, 2 };
+
+            // Llamada al método de ordenación
+            OrdenarBurbuja(sec);
+
+            // Salida
+            foreach (int num in sec)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+        public static void OrdenarBurbuja(int[] sec)
+        {
+            for (int i = 0; i < sec.Length; i++)
+            {
+                for (int j = i + 1; j < sec.Length; j++)
+                {
+                    if (sec[i] > sec[j])
+                    {
+                        int aux = sec[i];
+                        sec[i] = sec[j];
+                        sec[j] = aux;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ sumaTresNumerosOrdenados/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace sumaTresNumerosOrdenados
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n1 = 5;
+            int n2 = 7;
+            int n3 = 2;
+
+            bool esSuma = VerificarSuma(n1, n2, n3);
+            Console.WriteLine("La suma de los dos números menores es igual al mayor: " + esSuma);
+        }
+
+        public static bool VerificarSuma(int n1, int n2, int n3)
+        {
+            // Primero se ordenan los números
+            if (n2 < n1)
+            {
+                int aux = n1;
+                n1 = n2;
+                n2 = aux;
+            }
+
+            if (n3 < n1)
+            {
+                int aux = n1;
+                n1 = n3;
+                n3 = n2;
+                n2 = aux;
+            }
+            else if (n3 < n2)
+            {
+                int aux = n2;
+                n2 = n3;
+                n3 = aux;
+            }
+
+            // Verificar si la suma de los dos menores es igual al mayor
+            if (n1 + n2 == n3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
